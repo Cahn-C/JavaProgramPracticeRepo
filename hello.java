ca38fcced5728587c9c931054043434e259f24e4
Chaanyah Laborde <laborde.chaanyah@gmail.com>
2022-09-28 22:51:44 -0400

    Multiply 10 to the for loo[ eqaution

--- hello.java
+++ hello.java
@@ -3,7 +3,9 @@ public class hello  {
     public static  void main(String[] args) {
 
         for (int i = 0; i <= 10; i++) {
-            System.out.println("Hello World");
+            for (int j = 0; j <=10; j++) {
+                System.out.println(i * j + " ");
+            }
         }
 
     }
