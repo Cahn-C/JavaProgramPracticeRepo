0c7a3583f30918bcb8c77f8415a20209d573582e
Chaanyah Laborde <laborde.chaanyah@gmail.com>
2022-09-28 22:54:04 -0400

    Multiply repo by 11

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@ public class hello  {
 
     public static  void main(String[] args) {
 
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j <=10; j++) {
+        for (int i = 0; i <= 11; i++) {
+            for (int j = 0; j <=11; j++) {
                 System.out.println(i * j + " ");
             }
         }
